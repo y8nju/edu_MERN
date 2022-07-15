e809eec191a9ce5cc6e25b5dd0748983df128dd8
y8nju <89aruc@gmail.com>
2022-07-16 00:36:20 +0900

    Add: 방명록 수정하기 비밀번호 체크

--- nodeJs/ex03/router/visitor.js
+++ nodeJs/ex03/router/visitor.js
@@ -12,38 +12,36 @@ router.use((req, res, next) => {
 	console.log('log by anonymous');
 	next();
 });
-router.get('/', (req, res) => {
-	// new mongodb.ObjectId();
-	res.render('write');
+
+router.get('/', async (req, res) => {
+	res.redirect('/list')
+});
+router.get('/list', async (req, res) => {
+	let arr = await visiteBook.findAll();
+	res.render('list', {arr});
 });
 router.route('/write')
 	.get((req, res) => {
 		res.render('write');
 	})
 	.post( async (req, res) => {
-		const visite = {
+		const arr = {
 			name : req.body.name,
 			password : req.body.password,
 			comment: req.body.comment,
 			date: new Date()
 		};
 		
-		let result = await visiteBook.insertOne(visite);
+		let result = await visiteBook.insertOne(arr);
 		console.log(result);
 
 		if(result.insertedId) {
-			res.render('writeChk', {visite});
+			res.render('writeChk', {arr});
 			console.table(req.body)
 		} else {
 			res.render('write');
 		}
 	});
-
-router.get('/list', async (req, res) => {
-	let arr = await visiteBook.findAll();
-	res.render('list', {arr});
-});
-
 
 router.route('/delete')
 	.get(async (req, res) => {
@@ -62,28 +60,30 @@ router.route('/delete')
 			res.render('passwordChk', {visite, activeUrl, msg: "비밀번호가 일치하지 않습니다"});	
 		}
 	});
+
 router.route('/update')
 	.get(async (req, res) => {
 		let visite = await visiteBook.findById(req.query.id);
-		res.render('update', {visite});
+		res.render('update', {visite, msg: ""});
 		console.table(visite);
 	})
 	.post( async (req, res) => { 
-		let visite = {
+		let visite = await visiteBook.findById(req.body.id);
+		let arr = {
 			_id: req.body.id,
 			name : req.body.name,
 			password : req.body.password,
 			comment: req.body.comment,
 			date: req.body.date
 		};
-		
-		let result = await visiteBook.updateById(req.body.id, visite);
-		
-		res.render('writeChk', {visite});
-		
-		// res.redirect('list');
-		console.log(req.body.id);
-		console.table(visite);
+		console.log(visite.password, arr.password);
+		if(visite.password === arr.password) {
+			// let result = await visiteBook.updateById(req.body.id, arr);
+			res.render('writeChk', {visite, arr});
+		}else {
+			res.render('update', {visite, msg: "비밀번호가 일치하지 않습니다"});
+		}
+		console.table(arr);
 	});
 
 module.exports = router;
