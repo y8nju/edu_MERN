8398e9a1b4b77984ab4e17d3ecf9b5cedcbfa862
y8nju <107169943+y8nju@users.noreply.github.com>
2022-08-19 16:42:05 +0900

    Add: 채팅 회원가입

--- nodeJs/ex06_Mobile/Models/accounts.js
+++ nodeJs/ex06_Mobile/Models/accounts.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const accountSchema = new mongoose.Schema({
-	
+	id: String, 
+    password: String,
+    email: String
 })
 module.exports = mongoose.model('chatAccount', accountSchema)
--- nodeJs/ex06_Mobile/Router/accountRoute.js
+++ nodeJs/ex06_Mobile/Router/accountRoute.js
@@ -20,34 +20,38 @@ router.route('/login')
 		// }
 	})
 	
-router.route('/signup')
-	.get((req, res) => {
-		res.render('accounts/signup', {msg: ""})
-	})
-	.post(async (req, res) => {
-		const accuontData = {
-			id: req.body.id,
-			password: req.body.password,
-			email: req.body.email
-		}
-		let getId = await Account.findById(req.body.id);
-		if(!getId){
-			await Account.insertOne(accuontData);
-			// console.table(result)
-			res.render('accounts/signComplete', {accuontData});
-		}
-	})
 
 router.get("/idCheck", async(req, res) => {
-	let found = await Account.find({id: req.query.id});
+	let getId = await Account.findOne({id: req.query.id});
 	console.log(req.query.id)
+	console.log(getId)
 	const obj = {};
-	if(!found) {
+	if(!getId) {
 		obj.success = true;
 	}else {
 		obj.success = false;
 	}
 	res.json(obj);
 })
+router.route('/signup')
+	.get((req, res) => {
+		res.render('accounts/signup', {msg: ""})
+	})
+	.post(async (req, res) => {
+		let getId = await Account.findOne({id: req.query.id});
+		if(!getId){
+			let result = await Account.create({
+				id: req.body.id,
+				password: req.body.password,
+				email: req.body.email
+			});
+			console.log(result);
+			req.session.auth = true
+			req.session.userId = req.body.id
+			res.redirect('/chats');
+		}
+		//등록된 아이디가 있는 경우, 회원가입 버튼 disabled 처리해놓기
+		//등록된 아이디가 있는 경우, 회원가입 버튼 disabled 처리해놓기
+	})
 
 module.exports = router;
